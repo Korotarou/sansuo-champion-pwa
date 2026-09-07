8524fee08d72a7b0210ff69f6b83bd9333400873
Korotarou <masashi909@hotmail.com>
2026-09-07 13:25:18 +0900

    Fix SocialLab offline cache

--- social/sw.js
+++ social/sw.js
@@ -1,5 +1,5 @@
-const CACHE='social-lab-418-v02-20260907';
-const ASSETS=['./','./index.html','./styles.css','./data.js','./problem-engine.js','./app.js','./manifest.webmanifest','./assets/icon.svg','./assets/icon-192.png','./assets/icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match('./index.html'))))});
+const CACHE="social-lab-418-v02-20260907-p1";
+const ASSETS=["./","./index.html","./styles.css","./loader.js","./problem-engine.js","./app.js","./manifest.webmanifest","./assets/icon.svg","./payload/data1.txt","./payload/data2.txt","./payload/data3.txt"];
+self.addEventListener("install",function(e){e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(ASSETS);}).then(function(){return self.skipWaiting();}));});
+self.addEventListener("activate",function(e){e.waitUntil(caches.keys().then(function(keys){return Promise.all(keys.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));}).then(function(){return self.clients.claim();}));});
+self.addEventListener("fetch",function(e){if(e.request.method!=="GET")return;e.respondWith(caches.match(e.request).then(function(hit){if(hit)return hit;return fetch(e.request).then(function(res){var copy=res.clone();caches.open(CACHE).then(function(c){c.put(e.request,copy);});return res;}).catch(function(){return caches.match("./index.html");});}));});
