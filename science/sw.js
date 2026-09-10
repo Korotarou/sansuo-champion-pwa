681e8898acb53c8595d3f25a839b740e8d82acf9
Korotarou <masashi909@hotmail.com>
2026-09-10 13:50:10 +0900

    Cache science v07 assets

--- science/sw.js
+++ science/sw.js
@@ -1,8 +1,8 @@
-const CACHE = 'hayabusa-science-v6';
+const CACHE = 'hayabusa-science-v7';
 const ASSETS = [
   './','./index.html','./manifest.webmanifest','./assets/icon.svg',
   './styles-base.css','./styles-quiz.css','./styles-record.css',
-  './questions-1.js','./questions-2.js','./questions-3.js','./questions-4.js','./questions-5.js','./questions-6.js','./questions-7.js',
+  './questions-1.js','./questions-2.js','./questions-3.js','./questions-4.js','./questions-5.js','./questions-6.js','./questions-7.js','./questions-8.js','./sapix-import.js',
   './app-core.js','./app-quiz.js','./app-results.js','./app-boot.js'
 ];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())));
