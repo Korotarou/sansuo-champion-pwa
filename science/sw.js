bd64ea8562516ac210271b3d289712ff159b14a9
Korotarou <masashi909@hotmail.com>
2026-09-10 13:55:34 +0900

    Bump science offline cache v08

--- science/sw.js
+++ science/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'hayabusa-science-v7';
+const CACHE = 'hayabusa-science-v8';
 const ASSETS = [
   './','./index.html','./manifest.webmanifest','./assets/icon.svg',
   './styles-base.css','./styles-quiz.css','./styles-record.css',
